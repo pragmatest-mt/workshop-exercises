58fb54e38e61ad1f67da2938bb6e07f10a3a6f33
Clyde Vassallo <clydevassallo93@gmailcom>
2020-03-14 17:33:14 +0100

    Fix bug in chosen profile for UserRepo tests

--- continuous-testing-foundations/java/src/test/java/com/pragmatest/repositories/UserRepositoryIntegrationTest.java
+++ continuous-testing-foundations/java/src/test/java/com/pragmatest/repositories/UserRepositoryIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class UserRepositoryIntegrationTest {
 
     @Autowired
