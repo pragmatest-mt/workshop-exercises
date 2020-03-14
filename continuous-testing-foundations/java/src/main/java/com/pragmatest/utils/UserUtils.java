3aa74b17b991f199029035728a4e0cdd3fcc6088
Clyde Vassallo <clydevassallo93@gmailcom>
2020-03-14 17:32:35 +0100

    Replace IllegalArgumentException with UserInvalid

--- continuous-testing-foundations/java/src/main/java/com/pragmatest/utils/UserUtils.java
+++ continuous-testing-foundations/java/src/main/java/com/pragmatest/utils/UserUtils.java
@@ -1,12 +1,15 @@
 package com.pragmatest.utils;
 
+import com.pragmatest.exceptions.UserInvalidException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserUtils {
 
     public boolean isAdult(int age) {
-        if (age == 0) throw new IllegalArgumentException();
+        if (age <= 0) {
+            throw new UserInvalidException("Age must be more than 0.");
+        }
 
         return (age >= 18);
     }
