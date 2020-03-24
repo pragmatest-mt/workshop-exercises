d4630f802dbc4d45b611d75b1885306f526a881b
Andrea Mangion <andrea.mangion@gig.com>
2020-03-25 00:11:39 +0100

    Minor refactoring

--- continuous-testing-foundations/java/src/test/java/com/pragmatest/controllers/UserControllerIntegrationMockMvcTest.java
+++ continuous-testing-foundations/java/src/test/java/com/pragmatest/controllers/UserControllerIntegrationMockMvcTest.java
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UserControllerIntegrationTestMockMvc {
+public class UserControllerIntegrationMockMvcTest {
 
     private static final ObjectMapper om = new ObjectMapper();
 
